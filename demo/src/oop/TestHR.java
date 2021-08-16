3786d153cba5b5b084497dc7ed647220958f997f
srikanthpragada <srikanthpragada@gmail.com>
2021-08-16 18:56:33 +0530

    16-AUG

---
+++ demo/src/oop/TestHR.java
@@ -0,0 +1,16 @@
+package oop;
+
+import hr.Department;
+import hr.Employee;
+
+public class TestHR {
+
+	public static void main(String[] args) {
+		Employee e;  
+        // Job j;
+		
+		e = new Employee();
+		var d = new Department(); 
+	}
+
+}
