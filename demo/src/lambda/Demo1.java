95afecc4eeabfbf75515ad9997bd48579fd9e789
srikanthpragada <srikanthpragada@gmail.com>
2021-09-01 18:56:08 +0530

    01-SEP

--- demo/src/lambda/Demo1.java
+++ demo/src/lambda/Demo1.java
@@ -9,28 +9,36 @@ class Task implements Runnable {
 public class Demo1 {
 
 	public static void printNumbers() {
-		 for(int i  = 1; i <= 5; i ++)
-             System.out.println(i);
+		for (int i = 1; i <= 5; i++)
+			System.out.println(i);
 	}
+
 	public static void main(String[] args) {
-         Thread t1  = new Thread(new Task());
-         t1.start();
-         
-         // Lambda Expression 
-         Thread t2 = new Thread( () -> System.out.println("Lambda Thread!"));
-         t2.start();
-         
-         // Lambda Block
-         Thread t3 = new Thread( () -> {
-        	   for(int i  = 1; i <= 5; i ++)
-                System.out.println(i);
-            }
-         );
-         t3.start();
-         
-         // Method Reference
-         Thread t4 = new Thread(Demo1::printNumbers);
-         t4.start();
+		Thread t1 = new Thread(new Task());
+		t1.start();
+
+		Thread nt = new Thread(new Runnable() {
+			public void run() {
+				System.out.println("Annonymous inner class thread");
+			}
+		});
+		nt.start();
+		
+
+		// Lambda Expression
+		Thread t2 = new Thread(() -> System.out.println("Lambda Thread!"));
+		t2.start();
+
+		// Lambda Block
+		Thread t3 = new Thread(() -> {
+			for (int i = 1; i <= 5; i++)
+				System.out.println(i);
+		});
+		t3.start();
+
+		// Method Reference
+		Thread t4 = new Thread(Demo1::printNumbers);
+		t4.start();
 	}
 
 }
