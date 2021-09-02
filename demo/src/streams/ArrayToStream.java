6f2c158879237284db4d7cd71696a6802bbec0e3
srikanthpragada <srikanthpragada@gmail.com>
2021-09-02 18:53:43 +0530

    02-SEP

--- demo/src/streams/ArrayToStream.java
+++ demo/src/streams/ArrayToStream.java
@@ -16,13 +16,13 @@ public class ArrayToStream {
 		String[] names = { "TypeScript","Visual Basic","Java", 
 				           "JavaScript", "Php", "Python","C#" };
 
-		// Arrays.stream(names).forEach(new PrintName());
+//		Arrays.stream(names).forEach(new PrintName());
 		
 //		Arrays.stream(names)
 //		      .forEach( v -> System.out.println(v.toUpperCase()));
 		
 		Arrays.stream(names)
-		  .filter(v -> v.length() > 3)     // intermediate op
+		  .filter(v -> v.length() > 3)     // intermediate op (Predicate : bool test(Object))
 		  .sorted()                        // intermediate op 
 		  .limit(3)                        // intermediate op 
 	      .forEach(System.out::println);   // terminal op
--- demo/src/streams/PrintMoreThanAvg.java
+++ demo/src/streams/PrintMoreThanAvg.java
@@ -2,20 +2,27 @@ package streams;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class PrintMoreThanAvg {
 
 	public static void main(String[] args) throws Exception {
-
-		Path p = Path.of("c:\\classroom\\oct8\\marks.txt");
+		Path p = Path.of("d:\\classroom\\marks.txt");
 
 		var avgMarks = 
 		   	  Files.lines(p)
-		     .mapToInt(v -> Integer.parseInt(v))  // Covert to int
+		     .filter(v -> Pattern.matches("\\d+",v))
+		     .mapToInt(v -> Integer.parseInt(v))   // Covert to int
 		     .average()       // OptionalDouble 
 		     .getAsDouble();
 		
 		System.out.println(avgMarks);
+		
+		Files.lines(p)
+		  .filter(v -> Pattern.matches("\\d+",v))
+		  .mapToInt(v -> Integer.parseInt(v))   // Covert to int
+		  .filter(v -> v > avgMarks)
+		  .forEach(System.out::println);
 		    
 	}
 
